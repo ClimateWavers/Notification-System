81992b8148b0f67d508726dc9dbebd735bf48d04
Climate wavers <climatewaver@gmail.com>
2023-11-10 08:53:22 +0100

    Create emailController.js

---
+++ controllers/emailController.js
@@ -0,0 +1,15 @@
+// controllers/emailController.js
+const Email = require('../model/emailModel');
+
+const sendEmailController = async (req, res) => {
+    try {
+        const info = await Email.sendEmail(req.body);
+        console.log('Email sent: ' + info.response);
+        res.send('Email sent successfully');
+    } catch (error) {
+        console.error('Error occurred: ', error);
+        res.status(500).send('Error sending email');
+    }
+};
+
+module.exports = { sendEmailController };
